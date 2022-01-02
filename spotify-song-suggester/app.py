eb362e75bc6c585118ac9ee45ea640dd25fbba3c
Austin Francis <austincfrancis98@gmail.com>
2022-01-01 20:30:37 -0800

    formatting

--- spotify-song-suggester/app.py
+++ spotify-song-suggester/app.py
@@ -1,3 +1,4 @@
+import re
 import pandas as pd
 from flask import Flask, render_template, request, redirect
 from os import getenv
@@ -7,22 +8,27 @@ from predict import Predict
 def create_app():
     app = Flask(__name__)
     
+    app.secret_key = getenv('SECRET_KEY')
+    
     PW = getenv('PW')
     URI = f'mongodb+srv://admin:{PW}@spotifydb.sfcpj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
-    @app.route('/')
+    @app.route('/', methods=['GET', 'POST'])
     def root():
         return render_template('base.html')
 
-    @app.route('/results', methods=['POST'])
+    @app.route('/results', methods=['GET', 'POST'])
     def results():
         if request.method == 'POST':
             mongo = Mongo(URI)
             predict = Predict()
 
             songname = request.form.get('search')
-
-            song_data = mongo.find_one({'name': songname}) 
+            
+            try: 
+                song_data = mongo.find_one({'name': songname.lower()})
+            except:
+                return redirect('/')
 
             df = pd.DataFrame.from_dict(song_data, orient='index')
 
@@ -35,12 +41,22 @@ def create_app():
             data = []
             songs = []
 
-            for i in predictions:
-                data.append(mongo.find_one({'record_number': int(i)}))
+            for i in predictions[1:]:
+                try:
+                    data.append(mongo.find_one({'record_number': int(i)}))
+                except:
+                    return redirect('/')
 
             for i in data:
-                songs.append(i['name'])
+                songs.append(re.sub(r"[A-Za-z]+('[A-Za-z]+)?",
+                lambda word: word.group(0).capitalize(),
+                i['name']))
+                songs.append(i['artists'])
 
         return render_template('results.html', songs=songs)
+
+    @app.route('/about')
+    def about():
+        return render_template('about.html')
     
     return app
